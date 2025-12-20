ef301d3bf6dc9db0067f81fbff385256ae507a01
Oussama Jarouid <oussamajarouide110@gmail.com>
2025-12-20 15:21:08 +0100

    feat(theme): add color system and dark/light mode support

--- frontend/src/features/store.ts
+++ frontend/src/features/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "./slices/authSlice";
+import themethemeReducer from "./slices/themeSlice";
 
 export const store = configureStore({
-    reducer: {
-        auth: authReducer,
-    },
-})
+  reducer: {
+    auth: authReducer,
+    theme: themethemeReducer,
+  },
+});
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,16 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { Provider } from 'react-redux'
-import { store } from './features/store'
-import { Toaster } from 'sonner'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./features/store";
+import { Toaster } from "sonner";
+import App from "./App.tsx";
+import "./index.css";
+import { ThemeProvider } from "./components/providers/ThemeProvider.tsx";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Toaster position='top-right' />
+    <Toaster position="top-right" />
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
